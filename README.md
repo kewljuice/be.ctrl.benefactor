7a4d8728149122f098290fddb14b3449c29aadc8
kewljuice <kewljuice@hotmail.com>
2015-11-16 17:12:11 +0100

    init files
    Created initial extension files

---
+++ README.md
@@ -0,0 +1,24 @@
+# CiviCRM extension: Benefactor
+
+Benefactor is a CiviCRM extension.
+- [Installation](#installation)
+
+***
+
+## Installation
+
+- You can directly clone to your CiviCRM extension directory using<br>
+```$ git clone https://github.com/kewljuice/be.ctrl.benefactor.git```
+
+- You can also download a zip file, and extract in your extension directory<br>
+```$ git clone https://github.com/kewljuice/be.ctrl.benefactor/archive/master.zip```
+
+- Configure CiviCRM Extensions Directory which can be done from<br>
+```"Administer -> System Settings -> Directories".```
+
+- Configure Extension Resource URL which can be done from<br>
+```"Administer -> System Settings -> Resource URLs".```
+
+- The next step is enabling the extension which can be done from<br> 
+```"Administer -> System Settings -> Manage CiviCRM Extensions".```
+
---
+++ benefactor.php
@@ -0,0 +1,127 @@
+<?php
+
+require_once 'benefactor.civix.php';
+
+/**
+ * Implementation of hook_civicrm_config
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_config
+ */
+function benefactor_civicrm_config(&$config) {
+  _benefactor_civix_civicrm_config($config);
+}
+
+/**
+ * Implementation of hook_civicrm_xmlMenu
+ *
+ * @param $files array(string)
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_xmlMenu
+ */
+function benefactor_civicrm_xmlMenu(&$files) {
+  _benefactor_civix_civicrm_xmlMenu($files);
+}
+
+/**
+ * Implementation of hook_civicrm_install
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_install
+ */
+function benefactor_civicrm_install() {
+  _benefactor_civix_civicrm_install();
+}
+
+/**
+ * Implementation of hook_civicrm_uninstall
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_uninstall
+ */
+function benefactor_civicrm_uninstall() {
+  _benefactor_civix_civicrm_uninstall();
+}
+
+/**
+ * Implementation of hook_civicrm_enable
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_enable
+ */
+function benefactor_civicrm_enable() {
+	// continue
+  _benefactor_civix_civicrm_enable();
+}
+
+/**
+ * Implementation of hook_civicrm_disable
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_disable
+ */
+function benefactor_civicrm_disable() {
+  // continue
+	_benefactor_civix_civicrm_disable();
+}
+
+/**
+ * Implementation of hook_civicrm_upgrade
+ *
+ * @param $op string, the type of operation being performed; 'check' or 'enqueue'
+ * @param $queue CRM_Queue_Queue, (for 'enqueue') the modifiable list of pending up upgrade tasks
+ *
+ * @return mixed  based on op. for 'check', returns array(boolean) (TRUE if upgrades are pending)
+ *                for 'enqueue', returns void
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_upgrade
+ */
+function benefactor_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
+  return _benefactor_civix_civicrm_upgrade($op, $queue);
+}
+
+/**
+ * Implementation of hook_civicrm_managed
+ *
+ * Generate a list of entities to create/deactivate/delete when this module
+ * is installed, disabled, uninstalled.
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_managed
+ */
+function benefactor_civicrm_managed(&$entities) {
+  _benefactor_civix_civicrm_managed($entities);
+}
+
+/**
+ * Implementation of hook_civicrm_caseTypes
+ *
+ * Generate a list of case-types
+ *
+ * Note: This hook only runs in CiviCRM 4.4+.
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_caseTypes
+ */
+function benefactor_civicrm_caseTypes(&$caseTypes) {
+  _benefactor_civix_civicrm_caseTypes($caseTypes);
+}
+
+/**
+ * Implementation of hook_civicrm_alterSettingsFolders
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_alterSettingsFolders
+ */
+function benefactor_civicrm_alterSettingsFolders(&$metaDataFolders = NULL) {
+  _benefactor_civix_civicrm_alterSettingsFolders($metaDataFolders);
+}
+
+/**
+ * buildform
+ */
+function benefactor_civicrm_buildForm($formName, &$form) {
+	// set buildForm
+}
+
+/**
+ * pagerun
+ */
+function benefactor_civicrm_pageRun( &$page ) {
+	// set pageRun
+}
+
+
+ 
