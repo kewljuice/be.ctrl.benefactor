c27a59013c0cbaab9b248979c6cf8d338595abb2
kewljuice <kewljuice@hotmail.com>
2015-11-16 17:37:22 +0100

    Integrated page
    Integrated the front-end page where the angular application should come.

---
+++ CRM/benefactor/Page/benefactor.php
@@ -0,0 +1,18 @@
+<?php
+
+require_once 'CRM/Core/Page.php';
+
+class CRM_benefactor_Page_benefactor extends CRM_Core_Page {
+  function run() {
+			
+		CRM_Utils_System::setTitle(ts('Benefactor'));
+		
+		$url = CRM_Utils_System::url() . "civicrm/ctrl/benefactor";
+		$this->assign('url', $url);		
+		
+		$content = 'Benefactor: hello world';
+		$this-> assign('content', $content);
+		
+		parent::run();
+  }
+}
---
+++ templates/CRM/benefactor/Page/benefactor.tpl
@@ -0,0 +1,5 @@
+{* Display a variable directly *}
+<div>
+	<h3>Benefactor ({$url})</h3>
+	{$content}
+</div>
